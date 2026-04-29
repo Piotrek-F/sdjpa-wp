e3b22059933feaea0e236d6021fdb0cef632876c
Piotr Sadłowski <piotr.f.sadlowski@gmail.com>
2026-04-29 17:01:52 +0200

    End code for lesson 182 "Refactor for Bi-Directional Association" - adding bidirectional relation between User and UserMeta.

--- src/main/java/guru/springframework/sdjpa/wp/domain/User.java
+++ src/main/java/guru/springframework/sdjpa/wp/domain/User.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.*;
 import org.hibernate.validator.constraints.URL;
 
 import java.sql.Timestamp;
+import java.util.Set;
 
 @Entity
 @Table(name = "wp_users", indexes = {
@@ -62,6 +63,18 @@ public class User {
     @Size(max = 250)
     @Basic(optional = false)
     private String displayName;
+
+    @OneToMany
+    @JoinColumn(name = "user_id")
+    private Set<UserMeta> userMetaSet;
+
+    public Set<UserMeta> getUserMetaSet() {
+        return userMetaSet;
+    }
+
+    public void setUserMetaSet(Set<UserMeta> userMetaSet) {
+        this.userMetaSet = userMetaSet;
+    }
 
     public Long getId() {
         return id;
